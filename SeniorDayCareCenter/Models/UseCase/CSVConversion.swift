39c93594941421874dbbc8b214c88169ead52b27
muranakar <p1211065@gmail.com>
2022-06-08 22:11:15 +0900

    csvファイル書き換え、コンマの位置修正必要だった。

--- SeniorDayCareCenter/Models/UseCase/CSVConversion.swift
+++ SeniorDayCareCenter/Models/UseCase/CSVConversion.swift
@@ -30,10 +30,14 @@ struct CsvConversion {
                 let revisonString = string.replacingOccurrences(of: "\"", with: "")
                 return revisonString
             }
-            guard array.count == 24 else { return }
+            guard array.count == 24 else {
+                print(array)
+                return
+
+            }
             csvLineTwoDimensional.append(revisionArray)
         }
-
+        
         // 二次元配列のString型を、共通型に変換
         csvLineTwoDimensional.forEach { array in
             let pediatricWelfareService = FacilityInformation(
@@ -59,86 +63,31 @@ struct CsvConversion {
 }
 
 enum ServiceType: CaseIterable {
-    case communityCareForDementiaSupportingResidents320
-    case specifiedInstitutionalResidentialCarFeeBasedNursingHome331
-    case specifiedFacilityResidentialCareLightCostSeniorCitizensHome332
-    case specifiedFacilityResidentialCareResidenceForTheElderlyWithServices334
-    case specifiedFacilityResidentLifeCareFeeBasedSeniorCitizenHomeExternalServiceUseType335
-    case specifiedFacilityResidentialCareLightCostSeniorCitizensHomeExternalServiceUseType336
-    case specifiedFacilityResidentialCareResidencesForTheElderlyWithServicesAndExternalServices337
-    case communityBasedSpecifiedInstitutionalResidentialCareFeeBasedNursingHomeForTheElderly361
-    case communityBasedSpecifiedInstitutionalCareForResidentsLightCostSeniorCitizenHomes362
-    case communityBasedSpecifiedInstitutionalCareForResidentsResidencesForTheElderlyWithServices364
-    case nursingCareFacilityForTheElderly510
-    case nursingCareHealthFacilityForTheElderly520
-    case nursingCareHospital530
-    case communityBasedLongTermCareWelfareFacilityForTheElderlyResidents540
+    case DayCare
+    case DayCareMedicalTreatmentDayCare
+    case DayCareRehabilitation
 }
 
 extension ServiceType {
     var stringJapanese: String {
         switch self {
-        case .communityCareForDementiaSupportingResidents320:
-            return "認知症対応型共同生活介護"
-        case .specifiedInstitutionalResidentialCarFeeBasedNursingHome331:
-            return "特）有料老人ホーム"
-        case .specifiedFacilityResidentialCareLightCostSeniorCitizensHome332:
-            return "特）軽費老人ホーム"
-        case .specifiedFacilityResidentialCareResidenceForTheElderlyWithServices334:
-            return "特）サービス付き高齢者向け住宅"
-        case .specifiedFacilityResidentLifeCareFeeBasedSeniorCitizenHomeExternalServiceUseType335:
-            return "特）有料老人ホーム・外部サービス利用型"
-        case  .specifiedFacilityResidentialCareLightCostSeniorCitizensHomeExternalServiceUseType336:
-            return "特）軽費老人ホーム・外部サービス利用型"
-        case .specifiedFacilityResidentialCareResidencesForTheElderlyWithServicesAndExternalServices337:
-            return "特）サービス付き高齢者向け住宅・外部サービス利用型"
-        case .communityBasedSpecifiedInstitutionalResidentialCareFeeBasedNursingHomeForTheElderly361:
-            return "地) 有料老人ホーム"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsLightCostSeniorCitizenHomes362:
-            return "地) 軽費老人ホーム"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsResidencesForTheElderlyWithServices364:
-            return "地) サービス付き高齢者向け住宅"
-        case .nursingCareFacilityForTheElderly510:
-            return "介護老人福祉施設"
-        case .nursingCareHealthFacilityForTheElderly520:
-            return "介護老人保健施設"
-        case .nursingCareHospital530:
-            return "介護療養型医療施設"
-        case .communityBasedLongTermCareWelfareFacilityForTheElderlyResidents540:
-            return "地域密着型介護老人福祉施設入居者生活介護"
+        case .DayCare:
+            return "通所介護"
+        case .DayCareMedicalTreatmentDayCare:
+            return "通所介護（療養通所介護）"
+        case .DayCareRehabilitation:
+            return "通所リハビリテーション"
         }
     }
 
     var fileName: String {
         switch self {
-        case .communityCareForDementiaSupportingResidents320:
-            return "320_認知症対応型共同生活介護"
-        case .specifiedInstitutionalResidentialCarFeeBasedNursingHome331:
-            return "331_特定施設入居者生活介護（有料老人ホーム）"
-        case .specifiedFacilityResidentialCareLightCostSeniorCitizensHome332:
-            return "332_特定施設入居者生活介護（軽費老人ホーム）"
-        case .specifiedFacilityResidentialCareResidenceForTheElderlyWithServices334:
-            return "334_特定施設入居者生活介護（サービス付き高齢者向け住宅）"
-        case .specifiedFacilityResidentLifeCareFeeBasedSeniorCitizenHomeExternalServiceUseType335:
-            return "335_特定施設入居者生活介護（有料老人ホーム・外部サービス利用型）"
-        case .specifiedFacilityResidentialCareLightCostSeniorCitizensHomeExternalServiceUseType336:
-            return "336_特定施設入居者生活介護（軽費老人ホーム・外部サービス利用型）"
-        case .specifiedFacilityResidentialCareResidencesForTheElderlyWithServicesAndExternalServices337:
-            return "337_特定施設入居者生活介護（サービス付き高齢者向け住宅・外部サービス利用型）"
-        case .communityBasedSpecifiedInstitutionalResidentialCareFeeBasedNursingHomeForTheElderly361:
-            return "361_地域密着型特定施設入居者生活介護（有料老人ホーム）"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsLightCostSeniorCitizenHomes362:
-            return "362_地域密着型特定施設入居者生活介護（軽費老人ホーム）"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsResidencesForTheElderlyWithServices364:
-            return "364_地域密着型特定施設入居者生活介護（サービス付き高齢者向け住宅）"
-        case .nursingCareFacilityForTheElderly510:
-            return "510_介護老人福祉施設"
-        case .nursingCareHealthFacilityForTheElderly520:
-            return "520_介護老人保健施設"
-        case .nursingCareHospital530:
-            return "530_介護療養型医療施設"
-        case .communityBasedLongTermCareWelfareFacilityForTheElderlyResidents540:
-            return "540_地域密着型介護老人福祉施設入居者生活介護"
+        case .DayCare:
+            return "150_通所介護"
+        case .DayCareMedicalTreatmentDayCare:
+            return "155_通所介護（療養通所介護）"
+        case .DayCareRehabilitation:
+            return "160_通所リハビリテーション"
         }
     }
 }
--- SeniorDayCareCenter/Models/UseCase/SearchUseCase.swift
+++ SeniorDayCareCenter/Models/UseCase/SearchUseCase.swift
@@ -33,54 +33,21 @@ struct UseCaseSearch {
 
 enum FilterServiceType: CaseIterable {
     case all
-    case communityCareForDementiaSupportingResidents320
-    case specifiedInstitutionalResidentialCarFeeBasedNursingHome331
-    case specifiedFacilityResidentialCareLightCostSeniorCitizensHome332
-    case specifiedFacilityResidentialCareResidenceForTheElderlyWithServices334
-    case specifiedFacilityResidentLifeCareFeeBasedSeniorCitizenHomeExternalServiceUseType335
-    case specifiedFacilityResidentialCareLightCostSeniorCitizensHomeExternalServiceUseType336
-    case specifiedFacilityResidentialCareResidencesForTheElderlyWithServicesAndExternalServices337
-    case communityBasedSpecifiedInstitutionalResidentialCareFeeBasedNursingHomeForTheElderly361
-    case communityBasedSpecifiedInstitutionalCareForResidentsLightCostSeniorCitizenHomes362
-    case communityBasedSpecifiedInstitutionalCareForResidentsResidencesForTheElderlyWithServices364
-    case nursingCareFacilityForTheElderly510
-    case nursingCareHealthFacilityForTheElderly520
-    case nursingCareHospital530
-    case communityBasedLongTermCareWelfareFacilityForTheElderlyResidents540
+    case DayCare
+    case DayCareMedicalTreatmentDayCare
+    case DayCareRehabilitation
 }
 extension FilterServiceType {
     var string: String {
         switch self {
         case .all:
             return "全てのサービス"
-        case .communityCareForDementiaSupportingResidents320:
-            return "認知症対応型共同生活介護"
-        case .specifiedInstitutionalResidentialCarFeeBasedNursingHome331:
-            return "特）有料老人ホーム"
-        case .specifiedFacilityResidentialCareLightCostSeniorCitizensHome332:
-            return "特）軽費老人ホーム"
-        case .specifiedFacilityResidentialCareResidenceForTheElderlyWithServices334:
-            return "特）サービス付き高齢者向け住宅"
-        case .specifiedFacilityResidentLifeCareFeeBasedSeniorCitizenHomeExternalServiceUseType335:
-            return "特）有料老人ホーム・外部サービス利用型"
-        case  .specifiedFacilityResidentialCareLightCostSeniorCitizensHomeExternalServiceUseType336:
-            return "特）軽費老人ホーム・外部サービス利用型"
-        case .specifiedFacilityResidentialCareResidencesForTheElderlyWithServicesAndExternalServices337:
-            return "特）サービス付き高齢者向け住宅・外部サービス利用型"
-        case .communityBasedSpecifiedInstitutionalResidentialCareFeeBasedNursingHomeForTheElderly361:
-            return "地) 有料老人ホーム"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsLightCostSeniorCitizenHomes362:
-            return "地) 軽費老人ホーム"
-        case .communityBasedSpecifiedInstitutionalCareForResidentsResidencesForTheElderlyWithServices364:
-            return "地) サービス付き高齢者向け住宅"
-        case .nursingCareFacilityForTheElderly510:
-            return "介護老人福祉施設"
-        case .nursingCareHealthFacilityForTheElderly520:
-            return "介護老人保健施設"
-        case .nursingCareHospital530:
-            return "介護療養型医療施設"
-        case .communityBasedLongTermCareWelfareFacilityForTheElderlyResidents540:
-            return "地域密着型介護老人福祉施設入居者生活介護"
+        case .DayCare:
+            return "通所介護"
+        case .DayCareMedicalTreatmentDayCare:
+            return "通所介護（療養通所介護）"
+        case .DayCareRehabilitation:
+            return "通所リハビリテーション"
         }
     }
 }
