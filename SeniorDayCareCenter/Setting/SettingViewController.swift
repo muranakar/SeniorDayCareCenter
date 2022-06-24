b96f7c17c14a934e6ed0d73e232def6b49782354
muranakar <p1211065@gmail.com>
2022-06-24 19:35:29 +0900

    共有実装　version1.1.2

--- SeniorDayCareCenter/Setting/SettingViewController.swift
+++ SeniorDayCareCenter/Setting/SettingViewController.swift
@@ -31,6 +31,12 @@ class SettingViewController: UIViewController {
     @IBAction private func jumpToTwitter(_ sender: Any) {
         jumpToTwitterInformation()
     }
+    @IBAction private func shareTwitter(_ sender: Any) {
+        shareOnTwitter()
+    }
+    @IBAction private func shareLine(_ sender: Any) {
+        shareOnLine()
+    }
 
     private func selectRowFilterServiceTypePickerView() {
         guard let loadedFilterServiceType = filterServiceTypeRepository.load() else { return }
@@ -60,6 +66,59 @@ class SettingViewController: UIViewController {
         // 広告読み込み
         bannerView.load(GADRequest())
     }
+    func shareOnTwitter() {
+            // シェアするテキストを作成
+            let text = "全国のデイサービス・デイケアの検索が可能！"
+        // swiftlint:disable:next line_length
+            let hashTag = "#高齢者 #生活支援 #デイサービス #デイケア #リハビリ #理学療法　#作業療法  #介護保険 #医療保険 #在宅 #介護 #医療\nhttps://apps.apple.com/jp/app/%E3%83%87%E3%82%A4%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9-%E3%83%87%E3%82%A4%E3%82%B1%E3%82%A2map-%E5%85%A8%E5%9B%BD%E3%81%AE%E4%BA%8B%E6%A5%AD%E6%89%80%E3%82%92%E6%A4%9C%E7%B4%A2%E5%8F%AF%E8%83%BD/id1628660099"
+            let completedText = text + "\n" + hashTag
+
+            // 作成したテキストをエンコード
+            let encodedText = completedText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+
+            // エンコードしたテキストをURLに繋げ、URLを開いてツイート画面を表示させる
+            if let encodedText = encodedText,
+               let url = URL(string: "https://twitter.com/intent/tweet?text=\(encodedText)") {
+                UIApplication.shared.open(url)
+            }
+        }
+
+        func shareOnLine() {
+            let urlscheme: String = "https://line.me/R/share?text="
+            // swiftlint:disable:next line_length
+            let message = "全国のデイサービス・デイケアの検索が可能！#高齢者 #生活支援 #デイサービス #デイケア #リハビリ #理学療法　#作業療法  #介護保険 #医療保険 #在宅 #介護 #医療\nhttps://apps.apple.com/jp/app/%E3%83%87%E3%82%A4%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9-%E3%83%87%E3%82%A4%E3%82%B1%E3%82%A2map-%E5%85%A8%E5%9B%BD%E3%81%AE%E4%BA%8B%E6%A5%AD%E6%89%80%E3%82%92%E6%A4%9C%E7%B4%A2%E5%8F%AF%E8%83%BD/id1628660099"
+
+            // line:/msg/text/(メッセージ)
+            let urlstring = urlscheme + "/" + message
+
+            // URLエンコード
+            guard let  encodedURL =
+                    urlstring.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) else {
+                return
+            }
+
+            // URL作成
+            guard let url = URL(string: encodedURL) else {
+                return
+            }
+
+            if UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: { (succes) in
+                        //  LINEアプリ表示成功
+                    })
+                }else{
+                    UIApplication.shared.openURL(url)
+                }
+            }else {
+                // LINEアプリが無い場合
+                let alertController = UIAlertController(title: "エラー",
+                                                        message: "LINEがインストールされていません",
+                                                        preferredStyle: UIAlertController.Style.alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
+                present(alertController,animated: true,completion: nil)
+            }
+        }
 }
 
 extension SettingViewController: UIPickerViewDelegate {
